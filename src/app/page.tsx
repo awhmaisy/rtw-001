9a3504461c38c813527a0b7de62d22d6ffdba505
mei higashi <mei@meihigashi.com>
2024-12-28 18:09:37 -0800

    SS25 RTW 001 PIECES

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen font-['Times_New_Roman'] p-2">
+      <p>
+        USA SS25 RTW 001 <br/>
+        FORGED IN AMERICA <br/>
+        MFG. BY PRECISION MACHINERY
+      </p>
+      <br/>
+      <p style={{textDecoration: 'underline'}}>
+        COLLECTION INCLUDES
+      </p>
+      <p>
+        LETHALITY CALCULATIONS
+      </p>
+      <p>
+        IN MEMORY OF WHEN I GAVE A XXXX
+      </p>
+      <p>
+        PRIVATIZED INTELLIGENCE
+      </p>
+      <p>
+        BE VIGILANT, BE RETICENT , BE RELENTLESS
+      </p>
+      <p>
+        TACTICAL SIGHTS
+      </p>
+      <p>
+        SITREP
+      </p>
+      <p>
+        TARGET PACKAGE
+      </p>
+    </div>
+  );
+}
