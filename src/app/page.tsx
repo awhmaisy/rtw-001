65d969a12216c35c9995e5d35fd940d05cc60548
mei higashi <mei@meihigashi.com>
2025-01-07 12:59:43 -0800

    eva title screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import Items from './items/items';
 export default function Home() {
   return (
     <div className="min-h-screen font-['Times_New_Roman'] p-2">
+      <div className="w-full flex justify-center items-center mb-8">
+        <img src="/Evangelion Title Card.png" alt="Description" width={300} className="mr-auto" />
+      </div>
       <p>
         X-12, U.S.A. SS 2025, RTW 001 <br/>
         FORGED IN AMERICA <br/>
